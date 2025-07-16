7bcd616efe98e73e2f7990dfb15506b5b5314f48
ainvis <109056582+ainvishal@users.noreply.github.com>
2025-07-16 10:46:18 +0530

    sqlite added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -88,13 +88,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default':{
-        'ENGINE':'django.db.backends.mysql',
-        'NAME':'post_db',
-        'USER':'root',
-        'PASSWORD':'sql123',
-        'HOST':'localhost',
-        'PORT':'3306'
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
